9aac7e8dbac9e6efc6ca65cdc3a94a19d0096f11
dom <marsonowski14@gmail.com>
2020-07-10 20:21:45 +0200

    -addition previous completed tests

--- src/main/java/com/testapp/korepetycjedom/book/BookNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.testapp.korepetycjedom.book;
-
-public class BookNotFoundException extends Exception {
-
-    //stworzyłem swój wyjątek
-    public BookNotFoundException(Long id) {
-        super(String.format("Book %d doesn't exist!", id));
-    }
-}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.testapp.korepetycjedom;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KorepetycjeDomApplicationTests {
+class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
